44264ece7001042bf3c1441ef0d0e8019c3bf42d
kwang-s <parkks990@yonsei.ac.kr>
2020-08-12 22:01:31 +0900

    udemy-28

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,7 @@ void main() {
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://lh3.googleusercontent.com/proxy/EQnlzlEieEI9AKYR6GUITJ8PLaZ5-wfmm6U-pkehOWERR7-YX_Ha28HBn76Ug6KrfBR0e-iUlEDnlnaKEhyIl4z6JHAXPbJQWmxODe6mFw2YeL2tWw'),
-          ),
+          child: Image(image: AssetImage('images/turtle_1.jpg')),
         ),
       ),
     ),
